0c1b0c9045779889767f245c7a49e4faa58a984b
tanaka-py <goatbed.blitz.kiss@gmail.com>
2025-04-03 00:33:44 +0900

    送信のワードを強調表示

--- backend/app/services/notice.py
+++ backend/app/services/notice.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import asyncio
 import aiohttp
+import re
 from . import summarize
 
 # 重要度の高い文言検索
@@ -21,33 +22,38 @@ async def send(
     
     if not filterd_df.empty:
         
-        tasks_list = [summarize_send(row.Code, row.Name, row.Link) for row in filterd_df.itertuples()]
+        tasks_list = [summarize_send(row) for row in filterd_df.itertuples()]
         
         if tasks_list:
+            print('送信開始')
             # 並列で行う
             results = await asyncio.gather(*tasks_list, return_exceptions=True)
             for result in results:
                 if isinstance(result, Exception):
                     print(f"エラー発生：{result}")
+                    
+            print('全送信')
             
 # 要約して通知
 async def summarize_send(
-    code,
-    name,
-    link
+    row
 ):
     # 要約
-    text = summarize.summarize_long_document(link, summarize_limit=1000)
+    text = summarize.summarize_long_document(row.Link, summarize_limit=1000)
     
-    #　メッセージ作成
-    message={
-        'text': f'code:{code}\n社名:{name}\n{text}'
+    for ward in important_words:
+        text = re.sub(f'({ward})', r" `\1` ", text, flags=re.IGNORECASE)
+
+    # メッセージ作成（マークダウン形式）
+    payload = {
+        "text": f'*{row.Date} {row.Time}*  \n*code:* `{row.Code}`  \n---  \n*社名:* {row.Name}  \n{text}',
+        "mrkdwn": True  # ← これが超重要！！！！
     }
-    
+
     # 送信
     async with aiohttp.ClientSession() as session:
-        async with session.post(slack_url, json=message) as response:
+        async with session.post(slack_url, json=payload) as response:
             if response.status == 200:
-                print(f'code:{code} 社名:{name} 送信OK')
+                print(f'code:{row.Code} 社名:{row.Name} 送信OK')
             else:
-                print(f'code:{code} 社名:{name} 送信失敗')
+                print(f'code:{row.Code} 社名:{row.Name} 送信失敗')
